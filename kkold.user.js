fe4e20327b8792226a4b322e4b42db226afc8f6e
chainsawriot <chainsawtiney@gmail.com>
2022-07-02 23:10:35 +0200

    Use original CSS

--- kkold.user.js
+++ kkold.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         kkold
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Apply the old Koi Koi face without messing up other BGA games
 // @author       Chung-hong Chan
 // @match        https://boardgamearena.com/*/koikoi*
-// @resource     customCSS https://raw.githubusercontent.com/chainsawriot/kkold/master/BGA-Koi-Koi.css
+// @resource     customCSS https://gist.githubusercontent.com/gingerbeardman/119034acc7dd376692c21bfe01c4aebc/raw/47d963e617e8f3fbffadcb38fbe17aa1cdd490f0/BGA-Koi-Koi.css
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // ==/UserScript==
